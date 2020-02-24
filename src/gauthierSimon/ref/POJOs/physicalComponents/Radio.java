bb55bf8ca4c1226df03d0742fa063fd254008233
GausiVagos <38572600+GausiVagos@users.noreply.github.com>
2020-02-24 11:51:05 +0100

    Started moving the states objects to Radio

--- src/gauthierSimon/ref/POJOs/physicalComponents/Radio.java
+++ src/gauthierSimon/ref/POJOs/physicalComponents/Radio.java
@@ -5,10 +5,22 @@ import java.util.Map;
 
 public class Radio {
 	Map<String,RadioComponent> components;
+
+        OffState off;
+	OnState on;
+	PausedState paused;
+	PlayingState playing;
+	BrokenState broken;
 	
 	public Radio()// basic radio with no optional feature
 	{
-		components = new HashMap<>();
+	        off = new OffState(this);
+		on = new OnState(this);
+		paused = new PausedState(this);
+		playing = new PlayingState(this);
+		broken = new BrokenState(this);
+
+                components = new HashMap<>();
 		components.put("Primary speaker", new Speaker(100));
 		components.put("Antenna", new Antenna());
 	}
