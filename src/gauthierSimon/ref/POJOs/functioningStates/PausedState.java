4322e9ab474506a1f522673e4012937c294d1e62
GausiVagos <38572600+GausiVagos@users.noreply.github.com>
2020-02-24 08:34:11 +0100

    Simplified PausedState

--- src/gauthierSimon/ref/POJOs/functioningStates/PausedState.java
+++ src/gauthierSimon/ref/POJOs/functioningStates/PausedState.java
@@ -19,8 +19,7 @@ public class PausedState implements IState{
 
 	@Override
 	public boolean start() {
-		context.changeState(States.playing);
-		return true;
+		return context.changeState(States.playing);
 	}
 
 	@Override
@@ -30,14 +29,12 @@ public class PausedState implements IState{
 
 	@Override
 	public boolean turnOff() {
-		context.changeState(States.off);
-		return true;
+		return context.changeState(States.off);
 	}
 
 	@Override
 	public boolean breakComponent() {
-		context.changeState(States.broken);
-		return true;
+		return context.changeState(States.broken);
 	}
 
 	@Override
